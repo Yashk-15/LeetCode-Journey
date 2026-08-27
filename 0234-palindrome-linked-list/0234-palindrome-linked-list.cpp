e4de548fb70a32387524059c08c222f7e2c5cd03
Yash Kaushik <yashsk1505@gmail.com>
2026-08-27 23:06:16 +0530

    Time: 6 ms (29.67%), Space: 124.3 MB (34.36%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,41 +1,33 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* reverse(ListNode* head) {
-        ListNode* prev = nullptr;
-        ListNode* curr = head;
-        while (curr != nullptr) {
-            ListNode* next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
+
+ListNode* reverse(ListNode* head){
+    if(head == NULL || head->next == NULL)return head;
+    ListNode* newhead = reverse(head->next);
+    ListNode* front = head->next;
+    front->next = head;
+    head->next = NULL;
+    return newhead;
+}
+bool isPalindrome(ListNode* head) {
+    ListNode* slow = head;
+    ListNode* fast = head;
+    while(fast->next != NULL && fast->next->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
         }
-        return prev;
-    }
-    bool isPalindrome(ListNode* head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while (fast != nullptr && fast->next != nullptr) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        ListNode* rev = reverse(slow);
-        while (rev != nullptr) {
-            if (head->val != rev->val) {
+        ListNode* newhead = reverse(slow->next);
+        ListNode* first = head;
+        ListNode* second = newhead;
+        while(second != NULL){
+            if(first->val != second->val){
+                reverse(newhead);
                 return false;
             }
-            head = head->next;
-            rev = rev->next;
+            first = first->next;
+            second = second->next;
         }
+        reverse(newhead);
         return true;
     }
 };
