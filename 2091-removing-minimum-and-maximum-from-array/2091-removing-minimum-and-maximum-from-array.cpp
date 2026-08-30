80bed3d298d47aafa065e15d82b89e838ffa7f13
Yash Kaushik <yashsk1505@gmail.com>
2026-08-30 15:32:50 +0530

    Time: 0 ms (100%), Space: 92.2 MB (22.14%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -4,22 +4,17 @@ public:
         int minIndex = 0;
         int maxIndex = 0;
 
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] < nums[minIndex]) {
-                minIndex = i;
-            }
-
-            if (nums[i] > nums[maxIndex]) {
-                maxIndex = i;
-            }
+        for(int i = 0; i<nums.size(); i++){
+            if( nums[i] < nums[minIndex]) minIndex = i;
+            if(nums[i] > nums[maxIndex]) maxIndex = i;
         }
+        int left = min(minIndex, maxIndex);
+        int right = max(minIndex, maxIndex);
 
-        int removeFromFront = max(minIndex, maxIndex) + 1;
-        int removeFromBack = nums.size() - min(minIndex, maxIndex);
-        int removeFromBothSides = min(
-            minIndex + 1 + (nums.size() - maxIndex),
-            maxIndex + 1 + (nums.size() - minIndex)
-        );
-        return min(removeFromFront, min(removeFromBack, removeFromBothSides));
+        int front = right + 1;
+        int back = nums.size() - left;
+        int both = (left + 1) + (nums.size() - right);
+
+        return min(front, min(back,both));
     }
 };
