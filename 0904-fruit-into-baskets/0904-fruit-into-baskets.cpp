a5cc11e082ddb59ea5990f651c74a597bdb134ff
Yash Kaushik <yashsk1505@gmail.com>
2026-09-01 16:51:12 +0530

    Time: 23 ms (91.79%), Space: 84.7 MB (83.94%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -9,18 +9,25 @@ public:
         unordered_map<int, int> mpp;
 
         while (right < fruits.size()) {
+
             mpp[fruits[right]]++;
 
             while (mpp.size() > 2) {
+
                 mpp[fruits[left]]--;
+
                 if (mpp[fruits[left]] == 0) {
                     mpp.erase(fruits[left]);
-                    }
-                    left++;
-                    }
-                    maxlength = max(maxlength, right - left + 1);
-                    right++;
-                    }
-                    return maxlength;
-                    }
-                    };
+                }
+
+                left++;
+            }
+
+            maxlength = max(maxlength, right - left + 1);
+
+            right++;
+        }
+
+        return maxlength;
+    }
+};
